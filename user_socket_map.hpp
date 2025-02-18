6e2c3ce0b18b0ede60756711cd1a163c3d6fdaa5
Lyam Talbot <phrostphoenix@gmail.com>
2025-02-18 20:45:44 +1000

    Can now broadcast messages from one client to other clients. Have solved the problem of inter-thread communication.

---
+++ user_socket_map.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Lyam Talbot on 15/2/2025.
+//
+
+#ifndef USER_SOCKET_MAP_H
+#define USER_SOCKET_MAP_H
+#include "Poco/Net/StreamSocket.h"
+#include "im_app_structs.h"
+
+extern std::unordered_map<std::string, Poco::Net::StreamSocket*> user_socket_map;
+
+extern std::unordered_map<int, Poco::Net::StreamSocket*> port_socket_map;
+
+extern std::unordered_map<int, socket_data_struct_t*> socket_data_map;
+
+#endif //USER_SOCKET_MAP_H
